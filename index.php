3ecdec1534513f00450f9ffceb15885be37013bf
Avinash Kumar Singh <avinashkumarsingh.ignou@gmail.com>
2017-03-20 11:23:47 +0530

    Updload file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add Subscriber</title>
+</head>
+<body>
+	<form method="post" action="action.php"> 
+	<p><label>First Name: </label><input type="text" name="fname" /></p>
+	<p><label>Last Name: </label><input type="text" name="lname" /></p> 
+	<p><label>Email: </label><input type="text" name="email" /></p> 
+	<p><input type="submit" name="submit" value="SUBSCRIBE"/></p> 
+	</form> 
+</body>
+</html>
